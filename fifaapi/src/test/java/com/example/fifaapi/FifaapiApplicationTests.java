0d7461ece9ce556b8cddd2d87c30fed010c4a488
SangWoon123 <alrax3574@naver.com>
2023-03-10 21:51:47 +0900

    option

---
+++ fifaapi/src/test/java/com/example/fifaapi/FifaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fifaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
